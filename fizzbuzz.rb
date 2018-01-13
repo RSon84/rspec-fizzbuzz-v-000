54c8757478ffb28457b2452922557658b99b4e49
Devin Wilson <craigvin84@yahoo.com>
2018-01-13 19:25:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@ def fizzbuzz(int)
    return "FizzBuzz"
   end
 
-  if int % 5 == 0
+  elsif int % 5 == 0
    return "Buzz"
   end
 
-  if int % 3 == 0
+  elsif int % 3 == 0
    return "Fizz"
   end
 
