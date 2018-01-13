9d77df8f7b1119901db08eacd6bf931ac187b41d
Devin Wilson <craigvin84@yahoo.com>
2018-01-13 18:50:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz"
-  elsif int % 5 == 1
+  elsif int % 5 == 0
     "Buzz"
-  elsif int % (3 & 5) == (3 || 5)
+  elsif int % (3 & 5) == 0
     "FizzBUzz"
   else int % (3 || 5) != 0
     "nil"
